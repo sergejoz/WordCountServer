c33fb2891680b589cc46057f42ff7f1a25f52c12
Sergey Menshikov <43649713+sergejoz@users.noreply.github.com>
2020-06-16 23:12:31 +0500

    Изменил запрос, добавил скачивание файла

--- src/main/java/DatabaseHandler.java
+++ src/main/java/DatabaseHandler.java
@@ -1,5 +1,6 @@
-﻿import java.sql.*;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DatabaseHandler extends Configs {
     Connection dbConnection;
@@ -10,26 +11,27 @@ public class DatabaseHandler extends Configs {
         return dbConnection;
     }
 
-    // получение из БД всех директорий
-    public ArrayList<String> getPaths(String currentWord){
-        ArrayList<String> paths = new ArrayList<String>();
+    // получение из БД всех директорий и freq
+    public HashMap<String, Double> getPaths(String currentWord){
+        HashMap<String, Double> ResultMap = new HashMap<String,Double>();
         try{
-            String query = "select file_path from FileNames\n" +
+            String query = "select DISTINCT file_path, wordfreq from FileNames\n" +
             "inner join FileWords on FileWords.file_id = FileNames.file_id\n" +
             "inner join WordsFrequency on WordsFrequency.word_id = FileWords.word_id\n" +
-            "where word = ?";
+            "where word = ?\n" +
+                    "order by word_freq desc";
             PreparedStatement prSt = getDbConnection().prepareStatement(query);
             prSt.setString(1, currentWord);
             ResultSet resultSet = prSt.executeQuery();
             prSt.close();
             while(resultSet.next()){
-                paths.add(resultSet.getString("file_path"));
+                ResultMap.put(resultSet.getString("file_path"), Double.parseDouble(resultSet.getString("wordfreq")));
             }
         }
         catch (SQLException exception)
         {
             System.out.println(exception.getMessage());
         }
-        return paths;
+        return ResultMap;
     }
 }
--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -3,32 +3,62 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.PrintWriter;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @WebServlet(name = "MyServlet")
 public class MyServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         try {
             request.setCharacterEncoding("UTF-8");
             String word = request.getParameter("word");
             DatabaseHandler dbHandler = new DatabaseHandler();
-            ArrayList<String> pathsList = dbHandler.getPaths(word);
-            if (pathsList.size() != 0)
-                // возвращает линк на путь к файлу сразу с file:///
-                pathsList.forEach(k -> out.println("<a href=" + k + "\">" + k + "</a><br>"));
+            HashMap<String, Double> ResultMap = dbHandler.getPaths(word);
+            if (ResultMap.size() != 0)
+                ResultMap.forEach((k,v) -> {
+                    try {
+                        // при использовании прямых путей выдавало ошибку недопустимых символов в URL, поэтому добавил URLEncode
+                        out.println("<a href=find?path=" + URLEncoder.encode(k, "UTF-8") + ">" + k + ": " + v + " раз(а)" + "</a><br>");
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                });
             else
                 out.println("Файлы с таким словом не найдены!");
         } catch (Exception exception) {
             out.println("Произошла ошибка: \n" + exception);
         }
+    }
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = request.getParameter("path");
+        File file = new File(path);
+
+        /* сохраняет файл на диск, но нет анимации скачивания в браузере
+            String fileText =  Files.readAllLines(Paths.get(path)).toString();
+            String savePath = "E:\\" + file.getName();
+            Files.write(Paths.get(savePath), fileText.getBytes());
+*/
+
+        String fileText = Files.readAllLines(Paths.get(path)).toString();
+        response.setContentType("text/plain");
+        response.setHeader("Content-Disposition", "attachment; filename=" + file.getName());
+        try {
+            OutputStream outputStream = response.getOutputStream();
+            String outputResult = fileText;
+            outputStream.write(outputResult.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
 
