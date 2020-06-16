8473018dad1872a3e0448e3b2b9dbcec0eea2e2b
Sergey Menshikov <43649713+sergejoz@users.noreply.github.com>
2020-06-16 10:29:42 +0500

    Обновил запрос к БД
    вместо второго селекта добавил inner join, передал параметром переменную

--- src/main/java/DatabaseHandler.java
+++ src/main/java/DatabaseHandler.java
@@ -1,4 +1,4 @@
-import java.sql.*;
+﻿import java.sql.*;
 import java.util.ArrayList;
 
 public class DatabaseHandler extends Configs {
@@ -15,8 +15,11 @@ public class DatabaseHandler extends Configs {
         ArrayList<String> paths = new ArrayList<String>();
         try{
             String query = "select file_path from FileNames\n" +
-                    "inner join FileWords on FileWords.file_id = FileNames.file_id and FileWords.word_id = (select word_id from WordsFrequency where word = '" + currentWord + "')";
+            "inner join FileWords on FileWords.file_id = FileNames.file_id\n" +
+            "inner join WordsFrequency on WordsFrequency.word_id = FileWords.word_id\n" +
+            "where word = ?";
             PreparedStatement prSt = getDbConnection().prepareStatement(query);
+            prSt.setString(1, currentWord);
             ResultSet resultSet = prSt.executeQuery();
             prSt.close();
             while(resultSet.next()){
